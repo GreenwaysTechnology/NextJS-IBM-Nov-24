33b30c082981da4ca6e82e29b136d43a3a364d75
subugee <sasubramanian_md@hotmail.com>
2024-11-26 13:37:06 +0530

    next js getting started

---
+++ hello-world/src/app/layout.js
@@ -0,0 +1,15 @@
+
+
+
+export default function RootLayout({ children }) {
+
+  return <html lang="en">
+    <body>
+      {/* Insert page content */}
+      {/* {props.children} */}
+      <div id="rootLayout">
+        {children}
+      </div>
+    </body>
+  </html>
+}
